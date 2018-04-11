285da9560628eac245714499b6a488f7d1e37459
coreyboulet <cyrilcoaster@hotmail.com>
2018-04-10 22:06:46 -0400

    sixth

--- app.py
+++ app.py
@@ -19,22 +19,6 @@ def webhook():
 
   return "ok", 200
 
-{
-  "attachments": [],
-  "avatar_url": "http://i.groupme.com/123456789",
-  "created_at": 1302623328,
-  "group_id": "1234567890",
-  "id": "1234567890",
-  "name": "John",
-  "sender_id": "12345",
-  "sender_type": "user",
-  "source_guid": "GUID",
-  "system": false,
-  "text": "Hello world ☃☃",
-  "user_id": "1234567890"
-}
-
-
 
 def send_message(msg):
   url  = 'https://api.groupme.com/v3/bots/post'
