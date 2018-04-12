cb875417028952288fee212692b68c3e6f1ea30c
coreyboulet <cyrilcoaster@hotmail.com>
2018-04-11 21:57:28 -0400

    groupID2

--- app.py
+++ app.py
@@ -13,9 +13,17 @@ def webhook():
   data = request.get_json()
 
   # We don't want to reply to ourselves!
-  if data['group_id']==	'39961905' and data['name'] != 'Secretary of Coreyboulet':
+  # In this line I check that this message come from the alert channel so it starts the message telling 
+  #us about the raid happening. 
+  if data['group_id']==	'39995566' and data['name'] != 'Secretary of Coreyboulet':
 
     msg = '{}, announced :"{}".... Who is in ?'.format(data['name'], data['text'])
+    send_message(msg)
+
+  # Ici je verifie que on est bien dans le code du channel de conversation
+  #then I check the text and that I'm not talking to myself
+  elif data['group_id']=='39961905' and data['text']=="hello" and data['name'] != 'Secretary of Coreyboulet':
+  	msg = "Hello {}!".format(data['name'])
     send_message(msg)
 
   return "ok", 200
