6f8a749dc8a9a62bf1577db2bd2a6b50a6ea2174
coreyboulet <cyrilcoaster@hotmail.com>
2018-04-13 17:06:08 -0400

    First commit

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ import json
 
 from urllib.parse import urlencode
 from urllib.request import Request, urlopen
-from groupy.client import Client
+from groupy import attachments
 
 from flask import Flask, request
 
