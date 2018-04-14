ddf24ce642bde8ad44f7cfa2076d209278e81e65
coreyboulet <cyrilcoaster@hotmail.com>
2018-04-13 21:18:59 -0400

    First commit

--- app.py
+++ app.py
@@ -31,9 +31,9 @@ def webhook():
   	msg = 'Hello {}!'.format(data['name'])
   elif data['group_id']=='33797805' and mess=='good night' and data['name'] != 'Secretary of Coreyboulet':
   	msg = 'Sleep tight {}!'.format(data['name'])
-  elif data['group_id']=='39961905' and mess=='lol' and data['name'] != 'Secretary of Coreyboulet':
+  elif data['group_id']=='33797805' and mess=='lol' and data['name'] != 'Secretary of Coreyboulet':
   	msg = 'lol'
-  elif data['group_id']=='39961905' and data['text']=='@Rare' and data['name'] != 'Secretary of Coreyboulet':
+  elif data['group_id']=='33797805' and data['text']=='@Rare' and data['name'] != 'Secretary of Coreyboulet':
     msg = '@Abhinay Tirupati'
     #atch = 
    #message = 39961905.post(text='hi')
