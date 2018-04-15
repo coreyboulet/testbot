0d5a2ab748e6249035c95cc5b6f98b0b5818ffcd
coreyboulet <cyrilcoaster@hotmail.com>
2018-04-15 10:03:25 -0400

    First commit

--- app.py
+++ app.py
@@ -35,11 +35,7 @@ def webhook():
   	msg = 'lol'
   elif data['group_id']=='39961905' and data['text']=='@rare' and data['name'] != 'Secretary of Coreyboulet':
     msg = 'Hello, @Clare'
-    atch = {
-          'type':'mentions',
-          'user_ids':["27457002"],
-          'loci':[[7, 6]]  
-          }
+    atch = "{'type':'mentions','user_ids':["27457002"],'loci':[[7, 6]]}"
 
 
   send_message(msg)
