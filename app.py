db35785ee5a7a67af628ff8c62917850de0a1887
coreyboulet <cyrilcoaster@hotmail.com>
2018-04-15 10:34:26 -0400

    First commit

--- app.py
+++ app.py
@@ -18,22 +18,26 @@ def webhook():
   #Here I'm putting the message coming from the channel in lower so I dont care about caps
   mess = data['text']
   mess= mess.lower()
-  usrID=0
+
   
   # We don't want to reply to ourselves!
   # In this line I check that this message come from the alert channel so it starts the message telling 
   #us about the raid happening. 
   if data['group_id']==	'36731470' and data['name'] != 'Secretary of Coreyboulet':
     msg = '{}, announced :"{}".... Who is in ?'.format(data['name'], data['text'])
+    usrID=0
 
   # Ici je verifie que on est bien dans le code du channel de conversation
   #then I check the text and that I'm not talking to myself
   elif data['group_id']=='33797805' and mess=='hello' and data['name'] != 'Secretary of Coreyboulet':
   	msg = 'Hello {}!'.format(data['name'])
+    usrID=0
   elif data['group_id']=='33797805' and mess=='good night' and data['name'] != 'Secretary of Coreyboulet':
   	msg = 'Sleep tight {}!'.format(data['name'])
+    usrID=0
   elif data['group_id']=='39961905' and mess=='lol' and data['name'] != 'Secretary of Coreyboulet':
   	msg = 'lol'
+    usrID=0
   elif data['group_id']=='39961905' and data['text']=='@rare' and data['name'] != 'Secretary of Coreyboulet':
     msg = 'Hello, @Coreyboulet'
     usrID= 53626037
