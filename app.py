2a4c1cc4eb6ef902f3896a8c70940753ededb9d1
coreyboulet <cyrilcoaster@hotmail.com>
2018-04-15 12:50:24 -0400

    First commit

--- app.py
+++ app.py
@@ -40,7 +40,7 @@ def webhook():
   elif data['group_id']=='39961905' and mess=='lol' and data['name'] != 'Secretary of Coreyboulet':
     msg = 'lol this is cool, really'
     usrID= 53626037
-    locid= [0, 0]
+    locid= 0, 0
   elif data['group_id']=='39961905' and data['text']=='@rare' and data['name'] != 'Secretary of Coreyboulet':
     msg = 'Hello, @Coreyboulet @Abhinay @Matt'
     usrID= 35632718,53626037,20366614
