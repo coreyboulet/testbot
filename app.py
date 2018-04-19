a83ed3460e286358c0893065506e026dfb73f712
coreyboulet <cyrilcoaster@hotmail.com>
2018-04-18 23:52:23 -0400

    First commit

--- app.py
+++ app.py
@@ -59,7 +59,7 @@ def webhook():
     msg = 'A ghost was mentionned. @Rob'
     usrID= 18834490,0
     locid= [22,4],[0,0]
-  elif data['group_id']==os.getenv('GROUP_ID') and data['text']=='@Bot' and data['name'] != 'Secretary of Coreyboulet':
+  elif data['group_id']==os.getenv('GROUP_ID') and mess=='@bot' and data['name'] != 'Secretary of Coreyboulet':
     msg = "Hello Everyone, I'm a bot, please use me to notify people that need things on this channel. Right now, you can type @Ditto: Rob and Jackie, @Ghost: Rob @Rare: Mitch, Corey, Abhinay, Sabre, Sam-B @Quest: Mitch, Corey, Abhinay. Contact Corey to be added or deleted from a list " 
     usrID= 0,0
     locid= [0, 0],[0, 0]
