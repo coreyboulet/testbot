366cbaff2d4ef0a67441b0c1d5d8e5547a28eba0
coreyboulet <cyrilcoaster@hotmail.com>
2018-04-19 20:05:46 -0400

    First commit

--- app.py
+++ app.py
@@ -96,12 +96,13 @@ def send_message(msg, usrID, locid):
 
 
 
-def daily_message():
-  msg = "Hello Everyone, I'm a bot, please use me to notify people that need things on this channel. Right now, you can type @Ditto: Rob and Jackie, @Ghost: Rob @Rare: Mitch, Corey, Abhinay, Sabre, Sam-B @Quest: Mitch, Corey, Abhinay. Contact Corey to be added or deleted from a list " 
-  usrID= 0,0
-  locid= [0, 0],[0, 0]
-  schedule.every().day.at("20:03").do(send_message(msg, usrID, locid))
-  schedule.every().day.at("20:04").send_message(msg, usrID, locid)
+
+msg = "Hello Everyone, I'm a bot, please use me to notify people that need things on this channel. Right now, you can type @Ditto: Rob and Jackie, @Ghost: Rob @Rare: Mitch, Corey, Abhinay, Sabre, Sam-B @Quest: Mitch, Corey, Abhinay. Contact Corey to be added or deleted from a list " 
+usrID= 0,0
+locid= [0, 0],[0, 0]
+schedule.every().day.at("20:07").do(send_message(msg, usrID, locid))
+schedule.every().day.at("20:08").send_message(msg, usrID, locid)
+
 while True:
   schedule.run_pending()
   time.sleep(1)
