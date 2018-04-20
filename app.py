df33247e4491806a8cf1870a1d07ab70084de6dd
coreyboulet <cyrilcoaster@hotmail.com>
2018-04-19 20:01:58 -0400

    First commit

--- app.py
+++ app.py
@@ -93,13 +93,15 @@ def send_message(msg, usrID, locid):
   #request.add_header('content-type', 'application/json')
   #json = urlopen(request).read().decode()
 
-"""
+
+
+
 def daily_message():
   msg = "Hello Everyone, I'm a bot, please use me to notify people that need things on this channel. Right now, you can type @Ditto: Rob and Jackie, @Ghost: Rob @Rare: Mitch, Corey, Abhinay, Sabre, Sam-B @Quest: Mitch, Corey, Abhinay. Contact Corey to be added or deleted from a list " 
   usrID= 0,0
   locid= [0, 0],[0, 0]
-  schedule.every().day.at("23:42").do(send_message(msg, usrID, locid))
-  schedule.every().day.at("23:43").send_message(msg, usrID, locid)
+  schedule.every().day.at("20:03").do(send_message(msg, usrID, locid))
+  schedule.every().day.at("20:04").send_message(msg, usrID, locid)
 while True:
-    schedule.run_pending()
-    time.sleep(1)"""
+  schedule.run_pending()
+  time.sleep(1)
