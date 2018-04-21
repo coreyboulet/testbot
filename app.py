9b2472b0cd908f5b137ca04fd1d35f24f69cbd77
coreyboulet <cyrilcoaster@hotmail.com>
2018-04-21 16:36:41 -0400

    First commit

--- app.py
+++ app.py
@@ -15,7 +15,7 @@ scope=['https://spreadsheets.google.com/feeds']
 creds= ServiceAccountCredentials.from_json_keyfile_name('client_secret.json', scope)
 client = gspread.authorize(creds)
 
-
+sheet=client.open('GroupMeBot')
 
 
 
