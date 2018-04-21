b381b2a29c7f10f9788e26b1d7fb0283f2f6836a
coreyboulet <cyrilcoaster@hotmail.com>
2018-04-21 16:34:43 -0400

    First commit

--- app.py
+++ app.py
@@ -12,7 +12,7 @@ import gspread
 from oauth2client.service_account import ServiceAccountCredentials
 
 scope=['https://spreadsheets.google.com/feeds']
-creads= ServiceAccountCredentials.from_json_keyfile_name('client_secret.json', scope)
+creds= ServiceAccountCredentials.from_json_keyfile_name('client_secret.json', scope)
 client = gspread.authorize(creds)
 
 
