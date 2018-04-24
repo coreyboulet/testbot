daac16e3cbeaf91ba70d83dc2e8cff1060da985e
coreyboulet <cyrilcoaster@hotmail.com>
2018-04-24 07:16:09 -0400

    First commit

--- app.py
+++ app.py
@@ -49,13 +49,13 @@ def webhook():
     usrID= 0,0
     locid= [0, 0],[0, 0]
   elif data['group_id']==os.getenv('GROUP_ID') and 'lol' in mess and data['name'] != 'Secretary of Coreyboulet':
-    msg = sheet.cell(2,3).value
+    msg = 'lol'
     usrID= 0,0
     locid= [0, 0],[0, 0]
   elif data['group_id']==os.getenv('GROUP_ID') and '@rare' in mess and data['name'] != 'Secretary of Coreyboulet':
-    msg = 'Rare pokemon mentionned. @Coreyboulet @Abhinay @Mitch @Sabre @Sam @Harold @Allan'
-    usrID= 35632718,53626037,33632383,56662107,50236640,58375075 ,33612373
-    locid= [24,55],[0,0],[0,0],[0,0],[0,0],[0,0],[0,0]
+    msg = 'Rare pokemon mentionned. @Coreyboulet @Abhinay @Mitch @Sabre @Sam @Harold @Allan, @Celine'
+    usrID= 35632718,53626037,33632383,56662107,50236640,58375075 ,33612373,47762292
+    locid= [24,64],[0,0],[0,0],[0,0],[0,0],[0,0],[0,0],[0,0]
   elif data['group_id']==os.getenv('GROUP_ID') and '@quest' in mess and data['name'] != 'Secretary of Coreyboulet':
     msg = 'Quests were mentionned. @Coreyboulet @Abhinay @Mitch'
     usrID= 35632718,53626037,33632383
