340ade1252897b6ce96ee34970f22490a359f6de
coreyboulet <cyrilcoaster@hotmail.com>
2018-04-30 17:31:43 -0400

    First commit

--- app.py
+++ app.py
@@ -83,14 +83,14 @@ def webhook():
     msg = 'Quests were mentionned. @Coreyboulet @Abhinay @Mitch @Andee @Max @Allan @Celine'
     usrID= 35632718,53626037,33632383,41943092,58375075,33612373
     locid= [23,55],[0,0],[0,0],[0,0],[0,0],[0,0]
-  elif data['group_id']==os.getenv('GROUP_ID') and sheet.cell(2,1).value in mess and data['name'] != 'Secretary of Coreyboulet':
-    msg = sheet.cell(2,2).value
-    usrID= sheet.cell(2,3).value,0
-    locid= [sheet.cell(2,4).value,sheet.cell(2,5).value],[0, 0]
-  elif data['group_id']==os.getenv('GROUP_ID') and sheet.cell(3,1).value in mess and data['name'] != 'Secretary of Coreyboulet':
-    msg = sheet.cell(3,2).value
-    usrID= sheet.cell(3,3).value,0
-    locid= [sheet.cell(3,4).value,sheet.cell(3,5).value],[3, 0]
+#  elif data['group_id']==os.getenv('GROUP_ID') and sheet.cell(2,1).value in mess and data['name'] != 'Secretary of Coreyboulet':
+#    msg = sheet.cell(2,2).value
+#    usrID= sheet.cell(2,3).value,0
+#    locid= [sheet.cell(2,4).value,sheet.cell(2,5).value],[0, 0]
+#  elif data['group_id']==os.getenv('GROUP_ID') and sheet.cell(3,1).value in mess and data['name'] != 'Secretary of Coreyboulet':
+#    msg = sheet.cell(3,2).value
+#    usrID= sheet.cell(3,3).value,0
+#    locid= [sheet.cell(3,4).value,sheet.cell(3,5).value],[3, 0]
   elif data['group_id']==os.getenv('GROUP_ID') and '@ditto' in mess and data['name'] != 'Secretary of Coreyboulet':
     msg = 'Ditto was mentionned. @Rob @Clare @Izzy'
     usrID= 18834490,27457002,27100281
